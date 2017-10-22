34e2b7afdb4324ae4896f910980ce464fcce8e6c
Shane Pudner <setherith@gmail.com>
2017-10-22 12:48:26 +0100

    Video 4 - Basic Input Handling

--- src/setherith/game/Game.java
+++ src/setherith/game/Game.java
@@ -27,6 +27,7 @@ public class Game extends Canvas implements Runnable {
     private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
 
     private Screen screen;
+    public InputHandler input;
     
     public Game() {
         setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
@@ -48,6 +49,7 @@ public class Game extends Canvas implements Runnable {
     
     public void init() {
         screen = new Screen(WIDTH, HEIGHT, new SpriteSheet("/sprite_sheet.png"));
+        input = new InputHandler(this);
     }
     
     private synchronized void start() {
@@ -109,6 +111,19 @@ public class Game extends Canvas implements Runnable {
     
     public void tick() {
         tickCount++;
+        
+        if (input.up.isPressed()) { 
+            screen.yOffset--; 
+        }
+        if (input.down.isPressed()) { 
+            screen.yOffset++; 
+        }
+        if (input.left.isPressed()) { 
+            screen.xOffset--; 
+        }
+        if (input.right.isPressed()) { 
+            screen.xOffset++; 
+        }
     }
     
     public void render() {
